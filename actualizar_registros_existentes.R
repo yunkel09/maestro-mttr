6f9f5a0ddb9e70f58ab5c3a6fd7de8b75251c338
William Chavarria <wchavarria@tigo.com.gt>
2017-08-31 07:40:22 -0600

    borrar script regristros

--- actualizar_registros_existentes.R
+++
@@ -1,9 +0,0 @@
-#   ____________________________________________________________________________
-#   SCRIPT PARA GUARDAR Y/O ACTUALIZAR DATOS EN LA BASE DE DATOS            ####
-
-  # abrir conexion 
-  con       <-  odbcConnect(dsn = 'yunkel',
-                            uid = 'wchavarria',
-                            pwd = 'Tigo1234.')
-  
-  
